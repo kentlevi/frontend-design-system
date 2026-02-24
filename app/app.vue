49e87c7172f5371a93360dfdfb120fbd1a16b52c
Chaimel <chaimel.glophics@gmail.com>
2026-02-24 13:00:52 +0800

    config

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,37 @@
 <template>
     <div>
+        <!-- TEMPORARY -->
+        <div class="dev_site" v-if="is_dev">
+            <p>This is a staged site for {{ app_name }}</p>
+        </div>
         <NuxtLayout>
-            <NuxtPage/>
+            <NuxtPage />
         </NuxtLayout>
     </div>
 </template>
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+const env = String(config.public.ENV || '').toLowerCase()
+const is_dev = env === 'development' || env === 'homestead'
+
+const app_name = config.public.app_name
+</script>
+<style lang="scss" scoped>
+.dev_site {
+    min-height: 60px;
+    width: 100%;
+    background: #2E2E2E;
+    text-align: center;
+    position: sticky;
+    top: 0;
+    padding: 21px;
+    z-index: 102;
+    p {
+        color: #fff;
+        font-weight: 600;
+        line-height: 17px;
+        margin: 0;
+    }
+}
+</style>
