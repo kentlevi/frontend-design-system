978788c19945721641d5bc5a928ad1f73a2c9385
xxiao <sem.glophics@gmail.com>
2026-02-25 16:24:28 +0800

    standardize code style and formatting across project

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <p>Musticker</p>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<p>Musticker</p>
+		<NuxtRouteAnnouncer />
+		<NuxtWelcome />
+	</div>
 </template>
+
+<script lang="ts" setup>
+const unused = 123;
+
+console.log(unused);
+</script>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import stylistic from '@stylistic/eslint-plugin'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+	plugins: {
+		'@stylistic': stylistic
+	},
+	rules: {
+		'vue/no-multiple-template-root': 'off',
+		'vue/max-attributes-per-line': ['error', { singleline: 3 }],
+		'@stylistic/indent': ['error', 'tab'],
+		'vue/html-indent': ['error', 'tab'],
+		'@stylistic/no-tabs': 'off',
+		'no-unused-vars': 'error',
+		'eol-last': ['error', 'never'],
+		'no-trailing-spaces': 'error'
+	}
+})
