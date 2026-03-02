3a0083977816e4677461b7f1b68b08f0812a311d
xxiao <sem.glophics@gmail.com>
2026-03-02 16:08:02 +0800

    Product Page | Format fix

--- app/pages/[country]/stickers/[product].vue
+++ app/pages/[country]/stickers/[product].vue
@@ -1,12 +1,11 @@
+<template>
+	<ProductCategoryExperience category="stickers" />
+</template>
+
 <script setup lang="ts">
 import ProductCategoryExperience from '~/components/products/product-category/ProductCategoryExperience.vue';
 
 definePageMeta({
-    layout: 'home',
+	layout: 'home',
 });
 </script>
-
-<template>
-    <ProductCategoryExperience category="stickers" />
-</template>
-
