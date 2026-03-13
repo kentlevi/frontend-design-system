10a5abc8825193c2df6656554c86c7519ac34ca7
joshua.glophics <joshua.glophics@gmail.com>
2026-03-13 08:26:49 +0800

    Navigation refactor fetch

--- app/composables/auth/login/useLoginPageForm.ts
+++ app/composables/auth/login/useLoginPageForm.ts
@@ -105,6 +105,7 @@ export function useLoginPageForm(options: UseLoginPageFormOptions = {}) {
 		nonMemberEmailError.value = '';
 		nonMemberEmailHasError.value = false;
 		nonMemberOrderError.value = '';
+		guestVerificationError.value = '';
 		resendLimitReached.value = '';
 	}
 
