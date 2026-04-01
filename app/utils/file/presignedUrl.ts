023ecf17222f244afb1f77c08ee0617690d5c5e9
Aby <calagoaby.glophics@gmail.com>
2026-04-01 09:39:29 +0800

    refactor generation of presigned url

--- app/utils/file/presignedUrl.ts
+++ app/utils/file/presignedUrl.ts
@@ -1,36 +1,42 @@
-import axios from 'axios'
 import { getPresignedUrl } from '~/services/aws/presignedUrl.service'
-
-interface payload {
-	full_path: string,
-	file: File
-}
-
 
 /**
  * Get presigned url
  * Upload to S3 using presigned url and file_path
  */
 export async function uploadFileToPresignedUrl(
-	payload: payload
-): Promise<void> {
+	payload: { file_path_code: string, file: File }
+): Promise<{ file_name: string}> {
+
 	/** Request presigned upload url */
 	const presigned_response = await getPresignedUrl({
-		full_path: payload.full_path
+		file_path_code: payload.file_path_code,
+		image_type: payload.file.type
 	})
 
-	/** Get presigned url from response */
+	/** Get data from response */
 	const presigned_url = presigned_response.data?.presigned_url
+	const file_name = presigned_response.data?.file_name
 
 	/** Stop if presigned url was not returned */
 	if (!presigned_url) {
 		throw new Error('Presigned URL was not returned.')
 	}
+	/** Stop if full path or file name was not returned */
+	if (!file_name) {
+		throw new Error('File name was not resolved.')
+	}
 
 	/** Upload file to storage using the presigned url */
-	await axios.put(presigned_url, payload.file, {
+	await $fetch(presigned_url, {
+		method: 'PUT',
+		body: payload.file,
 		headers: {
 			'Content-Type': payload.file.type
 		}
 	})
+
+	return {
+		file_name,
+	}
 }
