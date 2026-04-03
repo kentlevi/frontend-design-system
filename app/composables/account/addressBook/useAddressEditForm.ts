f588be80fac4e8e0c544f5b5ff1aba3e5a590de1
Aby <calagoaby.glophics@gmail.com>
2026-04-03 14:19:41 +0800

    Musticker Development | Storefront | Customer Dashboard - Address Book – SF https://glophics-team-tejd16ze.atlassian.net/browse/GLOP-478
    [feat]
    - update address

--- app/composables/account/addressBook/useAddressEditForm.ts
+++ app/composables/account/addressBook/useAddressEditForm.ts
@@ -1,12 +1,14 @@
 import type { Ref } from 'vue';
+import { updateUserAddress } from '~/services/profile/address.service';
 import { useAddressFieldStore, useAddressStore } from "~/stores/address";
 import type { AddressFormState, AddressFormMap, AddressMap, AddressType, DynamicFieldDefinition } from "~/types/address";
 
 type UseAddressEditFormOptions = {
 	form_state: AddressFormState
 	form_type: Ref<AddressType>
+	active_form: ComputedRef<AddressFormMap[AddressType]>
 	openEditFormModal: () => void
-	closeModal: () => void
+	closeFormModal: () => void
 	setCreateMode: () => void
 	clearFormFieldErrors: () => void
 }
@@ -20,6 +22,7 @@ export function useAddressEditForm(options: UseAddressEditFormOptions) {
 	const address_field_store = useAddressFieldStore()
 	const address_store = useAddressStore()
 	const toast_store = useToastStore()
+	const loading_overlay_store = useLoadingOverlayStore()
 
 	const editing_address_id = ref<number | null>(null)
 	const editing_address_snapshot = ref<AddressMap[AddressType] | null>(null)
@@ -105,82 +108,96 @@ export function useAddressEditForm(options: UseAddressEditFormOptions) {
 	function openEditModal(address: AddressMap[AddressType]) {
 		editing_address_id.value = address.id
 		editing_address_snapshot.value = address
+
+		// 1) point active_form to the right bucket first
 		options.form_type.value = address.type
 
-		if (address.type === 'shipping') {
-			options.form_state.shipping = mapAddressToForm(address) as AddressFormMap['shipping']
-		}
+		// 2) map API address -> form shape
+		const mapped_form = mapAddressToForm(address)
 
-		if (address.type === 'billing') {
-			options.form_state.billing = mapAddressToForm(address) as AddressFormMap['billing']
-		}
-
-		if (address.type === 'drop') {
-			options.form_state.drop = mapAddressToForm(address) as AddressFormMap['drop']
-		}
+		// 3) write into the currently active form object
+		Object.assign(options.active_form.value, mapped_form)
 
 		options.clearFormFieldErrors()
 		options.openEditFormModal()
 	}
 
-	function updateAddressLocally() {
+	async function updateAddressLocally() {
 		if (editing_address_id.value === null || !editing_address_snapshot.value) {
-			options.closeModal()
+			options.closeFormModal()
 			return
 		}
+		options.closeFormModal()
+		startUpdateOverlay()
 
 		const type = options.form_type.value
 		const editing_address = editing_address_snapshot.value
+		const payload = { ...options.active_form.value };
 
-		if (type === 'drop') {
-			address_store.updateAddress(type, {
-				...editing_address,
-				...options.form_state.drop,
-				country_id: editing_address.country_id,
-				type: 'drop',
-			})
+		try {
+			const response = await updateUserAddress(editing_address_id.value, payload)
+
+			if (response.success) {
+				if (type === 'drop') {
+					address_store.updateAddress(type, {
+						...editing_address,
+						...options.form_state.drop,
+						country_id: editing_address.country_id,
+						type: 'drop',
+					})
+				}
+
+				if (type === 'billing') {
+					const { fields, ...billing_form } = options.form_state.billing
+
+					address_store.updateAddress(type, {
+						...editing_address,
+						...billing_form,
+						id: editing_address_id.value,
+						country_id: editing_address.country_id,
+						type: 'billing',
+						dynamic_fields: address_field_store.dynamic_address_fields.map(field => ({
+							...field,
+							value: String(fields[field.field_key] ?? ''),
+						})),
+					})
+				}
+
+				if (type === 'shipping') {
+					const { fields, ...shipping_form } = options.form_state.shipping
+
+					address_store.updateAddress(type, {
+						...editing_address,
+						...shipping_form,
+						id: editing_address_id.value,
+						country_id: editing_address.country_id,
+						type: 'shipping',
+						dynamic_fields: address_field_store.dynamic_address_fields.map(field => ({
+							...field,
+							value: String(fields[field.field_key] ?? ''),
+						})),
+					})
+				}
+				resetEditState()
+				options.setCreateMode()
+			} else {
+				options.openEditFormModal()
+			}
+
+			toast_store.handleApiResponse(response)
+		} catch (_error: unknown) {
+			console.log(_error);
+		} finally {
+			loading_overlay_store.stopLoading('update_address')
 		}
+	}
 
-		if (type === 'billing') {
-			const { fields, ...billing_form } = options.form_state.billing
-
-			address_store.updateAddress(type, {
-				...editing_address,
-				...billing_form,
-				id: editing_address_id.value,
-				country_id: editing_address.country_id,
-				type: 'billing',
-				dynamic_fields: address_field_store.dynamic_address_fields.map(field => ({
-					...field,
-					value: String(fields[field.field_key] ?? ''),
-				})),
-			})
-		}
-
-		if (type === 'shipping') {
-			const { fields, ...shipping_form } = options.form_state.shipping
-
-			address_store.updateAddress(type, {
-				...editing_address,
-				...shipping_form,
-				id: editing_address_id.value,
-				country_id: editing_address.country_id,
-				type: 'shipping',
-				dynamic_fields: address_field_store.dynamic_address_fields.map(field => ({
-					...field,
-					value: String(fields[field.field_key] ?? ''),
-				})),
-			})
-		}
-
-		options.closeModal()
-		options.setCreateMode()
-		resetEditState()
-		toast_store.showToastWithTimer({
-			message: 'Your address has been updated.',
-			tone: 'primary',
-			dismissible: true,
-			variant: 'default',
+	/** Overlays */
+	function startUpdateOverlay() {
+		loading_overlay_store.startLoading('update_address', {
+			showCopy: true,
+			testId: 'account-profile-upload-avatar-overlay',
+			position: 'fixed'
 		})
 	}
 
