5be7a59b7aed8326a43f59d4a0a8f7ee100e9b22
Mat Mitcheil <matmitcheil.glophics@gmail.com>
2026-05-18 15:32:05 +0800

    [feature] Checkout Flow Optimization

--- app/composables/payments/bank-transfer/useBankTransfer.ts
+++ app/composables/payments/bank-transfer/useBankTransfer.ts
@@ -1,8 +1,5 @@
-import { useAddressGeneral } from "~/composables/checkout/address/useAddressGeneral"
 import { useCheckoutCompletion } from "~/composables/checkout/completion/useCheckoutCompletion"
-import { completeCheckoutRequest } from "~/services/checkout/checkout.service"
 import { useMainCheckOutStore } from "~/stores/checkout/index.store"
-import { useCartStore } from "~/stores/cart"
 
 export const useBankTransfer = () => {
 
@@ -10,20 +7,9 @@ export const useBankTransfer = () => {
 		completeCheckout
 	} = useCheckoutCompletion({redirectPath: 'checkout/confirmation'})
 	const checkout_store = useMainCheckOutStore()
-	const {
-		selected_ids
-	} = storeToRefs(useCartStore())
-
-	/** Contexts */
-	const { buildCompleteCheckoutPayload } = useAddressGeneral()
 
 	const processBankTransfer = async (order_id:number) => {
 		try {
-			const payload = {
-				...buildCompleteCheckoutPayload(order_id),
-				selected_cart_ids : selected_ids.value
-			}
-			await completeCheckoutRequest(payload)
 			checkout_store.cleanCheckoutStates()
 			completeCheckout(true, order_id)
 		} catch (error) {
